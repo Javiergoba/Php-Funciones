da482e67c8e6cb80fc53745ff872dca5e455bc67
JavierGoba <joangoba36@gmail.com>
2023-04-12 10:17:13 +0200

    nivel 1 completo

--- Nivel 1/Ejerc6.php
+++ Nivel 1/Ejerc6.php
@@ -1,6 +1,22 @@
+Charlie em va mossegar el dit!
+
+Charlie et mossegarà el dit exactament el 50% del temps.
+
+Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari
 <?php
-$num = rand(1,100);
-echo $num;
+
+
+function isBitten(){
+    $num = rand(0,1);
+    if ($num == 0){
+    return "Me ha mordido";
+    }else{
+    return "Me he librado de que me muerda";
+}
+
+};
+
+echo "<h2>". isBitten() . "</h2>"."<br>";
 
 ?>
 
@@ -8,4 +24,4 @@ Charlie em va mossegar el dit!
 
 Charlie et mossegarà el dit exactament el 50% del temps.
 
-Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.+ç+ç+
+Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.
